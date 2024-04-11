fe6e05b43c276400fffceff0db778fbfdba8593a
amirasabdu <archabdulrm@gmail.com>
2024-04-11 04:44:20 +0000

    Insertion with separate swap function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the integer values of two list nodes
+ * @node1: Pointer to the first node
+ * @node2: Pointer to the second node
+ *
+ * Description: This function swaps the integer values of two list nodes.
+ */
+
+void swap(listint_t *node1, listint_t *node2)
+{
+	int temp = node1->n;
+
+	node1->n = node2->n;
+	node2->n = temp;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list using the
  * Insertion Sort algorithm
  * @list: Pointer to the head of the doubly linked list
@@ -19,20 +35,20 @@ void insertion_sort_list(listint_t **list)
 	for (current = (*list)->next; current; current = temp)
 	{
 		temp = current->next;
-
 		while (current->prev && current->prev->n > current->n)
 		{
-			current->prev->next = current->next;
-			if (current->next)
+			if (!current->next)
+				*list = current;
+			else
+			{
+				current->prev->next = current->next;
 				current->next->prev = current->prev;
-
+			}
 			current->next = current->prev;
 			current->prev = current->next->prev;
 			current->next->prev = current;
 
-			if (!current->prev)
-				*list = current;
-			else
+			if (current->prev)
 				current->prev->next = current;
 		}
 	}
